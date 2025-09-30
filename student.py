cce435301eadf2a2c6fab6760eeccca65948a545
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-09-30 09:27:24 +0530

    student details

---
+++ student.py
@@ -0,0 +1,10 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+usn=input("Enter your USN: ")
+course=input("Enter your course: ")
+
+print("Student Details:")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"USN: {usn}")
+print(f"Course: {course}")
